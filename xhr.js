7b847e44d540fd378da06b4fc0fe9b2219b65861
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2015-03-29 17:57:25 -0300

    first commit

---
+++ xhr.js
@@ -0,0 +1,13 @@
+function xhrSuccess () { this.callback.apply(this, this.arguments); }
+
+function xhrError () { console.error(this.statusText); }
+
+function loadFile (sURL, fCallback) {
+  var oReq = new XMLHttpRequest();
+  oReq.callback = fCallback;
+  oReq.arguments = Array.prototype.slice.call(arguments, 2);
+  oReq.onload = xhrSuccess;
+  oReq.onerror = xhrError;
+  oReq.open("get", sURL, true);
+  oReq.send(null);
+}
